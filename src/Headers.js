3c9ecff30a1f82b3c6178941a740030b7d96b8c6
Niki Heaney <32551966+NickSx8@users.noreply.github.com>
2020-10-13 17:36:57 +0100

    changed lower to upper

--- src/Headers.js
+++ src/Headers.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import UCPLogo  from './UCPLogo.png'
-import './headers.css';
+import './Headers.css';
 
 export default class Headers extends React.Component{
  render(){
      return (
             <header>   
-                
                     <img src={UCPLogo} className="UCPLogoBlue" />  
-                    
             </header>
      )
  }
