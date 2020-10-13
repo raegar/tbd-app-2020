59c9de5d6ebd7b1fb9ab66a6e6eb06192a98ea5e
Daniel Dicker <p110094643@student.peterborough.ac.uk>
2020-10-13 16:32:36 +0100

    Added a variety of buttons

---
+++ src/UCPButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './UCPButton.css';
+function UCPButton() {
+    return (
+        <div className="Button">
+
+<br></br>
+<form action="https://canvas.anglia.ac.uk/courses/17176/modules">
+<button className="button">Button 1</button>
+</form>
+<br></br>
+<form action="https://canvas.anglia.ac.uk/courses/17176/modules">
+<button className="button2">Button 2</button>
+</form>
+<br></br>
+<form action="https://canvas.anglia.ac.uk/courses/17176/modules">
+<button className="lightbutton">Button 3</button>
+</form>
+
+<br></br>
+<form action="https://canvas.anglia.ac.uk/courses/17176/modules">
+<button className="tealbutton">Button 4</button>
+</form>
+
+<br></br>
+<form action="https://canvas.anglia.ac.uk/courses/17176/modules">
+<button className="largetealbutton">Button 5</button>
+</form>
+</div>
+    )
+    }
+
+export default UCPButton;
