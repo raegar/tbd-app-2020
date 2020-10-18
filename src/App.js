132067da996fcccf9812432c69441c5f0dfa9e48
Daniel Dicker <p110094643@student.peterborough.ac.uk>
2020-10-18 18:18:09 +0100

    Sizing for the disclaimer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button2 from './UCPButton.js';
-
+import Disclaimer from './Disclaimer.js';
 function App() {
   return (
     <div className="App">
@@ -19,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Disclaimer></Disclaimer>
         <Button2></Button2>
 <br></br>
       </header>
---
+++ src/Disclaimer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Disclaimer.css';
+function Disclaimer() {
+    return (
+        <div className="Disclaimer">
+
+<br></br>
+<form action="https://canvas.anglia.ac.uk/courses/17176/modules">
+<div className="disclaimer">
+  </div>
+</form>
+
+</div>
+    )
+    }
+
+export default Disclaimer;
