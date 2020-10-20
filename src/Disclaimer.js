99b42536cffa6ab9f18f09966a24ebfa62c2c6ff
Daniel Dicker <p110094643@student.peterborough.ac.uk>
2020-10-20 16:37:10 +0100

    Altered the font and size

--- src/Disclaimer.js
+++ src/Disclaimer.js
@@ -5,9 +5,10 @@ function Disclaimer() {
         <div className="Disclaimer">
 
 <br></br>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap" rel="stylesheet"></link>
 <form action="https://canvas.anglia.ac.uk/courses/17176/modules">
 <div className="disclaimer">
-    <b>PRIVACY STATEMENT:</b><br></br> We will only use your personal information to administer your application and / or enrolment and to provide the products and services you have requested from us.<br></br> Further information on our privacy policy can be found at https://www.ucp.ac.uk/privacy-and-cookies/ or by emailing <a href="mailto:hello@ucp.ac.uk" className="text">hello@ucp.ac.uk</a></div>
+ <p>  <b>PRIVACY STATEMENT:</b><br></br> We will only use your personal information to administer your application and / or enrolment and to provide the products and services you have requested from us.<br></br> Further information on our privacy policy can be found at https://www.ucp.ac.uk/privacy-and-cookies/ or by emailing <a href="mailto:hello@ucp.ac.uk" className="text">hello@ucp.ac.uk</a></p> </div>
 </form>
 
 </div>
