65287b1daa6b830c02adb00342971dd2990b3530
Niki Heaney <32551966+NickSx8@users.noreply.github.com>
2020-10-27 17:15:58 +0000

    Login Validation Completed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,37 +4,103 @@ import TextBox from '../TextBox/TextBox'
 import UCPButton from '../Buttons/UCPButton';
 import './Login.css';
 
-export default class Login extends React.Component{
-    render(){
-        return (     
-            <Container className="ContainerStyle">
-                <Row>
-                    <div className="UserInfo">
-                        <h2>Login</h2>
-                    </div>
-                </Row>
-                <Row> 
-                    <Col>
-                        <div className="UserInfo">
-                            <p>Username: </p>
-                            <TextBox placeholder="Enter Username"/> 
-                        </div>  
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <div className="UserInfo">
-                            <p style={{paddingRight: "4px"}}>Password: </p>
-                            <TextBox placeholder="Enter Password" type="password"/> 
-                        </div>  
-                    </Col>
-                </Row>
-                <Row>
-                    <div className="UserInfo">
-                        <UCPButton buttonText="Login" className="smallbutton"/>
-                    </div>
-                </Row>
-            </Container>
-            )
+const initialState = {
+    username: "",
+    password: "",
+    usernameError: "",
+    passwordError: ""
+};
+
+export default class LoginForm extends React.Component {
+    state = initialState;
+
+    handleChangeUsername = event => {
+        this.setState({username: event.target.value });
+    };
+    handleChangePassword = event => {
+        this.setState({password: event.target.value });
+    };
+    
+    handleSubmit = event => {
+        event.preventDefault();
+        const isValid = this.validate();
+        if(isValid){
+            console.log(this.state);
+            this.setState(initialState);
         }
     };
+    validateEmail(email){
+        const pattern = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/;
+        const result = pattern.test(email);
+        if(result===true){
+          this.setState({
+            usernameError:false,
+            username:email
+          })
+          return true;
+        } else{
+          this.setState({
+            usernameError:true
+          })
+          return false;
+        }
+      };
+
+    validate = () => {
+        let usernameError = "";
+        let passwordError = "";
+        if(!this.validateEmail(this.state.username))
+        {
+            usernameError = "invalid username"  
+        }
+        if(!this.state.password)
+        {
+            passwordError = "invalid password"   
+        }
+        if(usernameError || passwordError)
+        {
+            this.setState({usernameError, passwordError});
+            return false;
+        }
+        return true;
+    };
+    ClearForm = () => {
+     this.setState(initialState);
+    };
+    render() {
+        return(
+            <Container className="ContainerStyle">
+                <form>
+                    <Row>
+                        <Col>
+                        <div className="UserInfo">
+                            <p>Username: </p>
+                            <TextBox type="text" value={this.state.username} 
+                             onChange={this.handleChangeUsername} placeholder="Enter Username"/> 
+                            <span style={{ fontSize: 10, color: "red"}}>{this.state.usernameError}</span>
+                        </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                        <div className="UserInfo">
+                            <p>Password: </p>
+                            <TextBox type="password" value={this.state.password} 
+                             onChange={this.handleChangePassword} placeholder="Enter Password"/> 
+                             <span style={{ fontSize: 10, color: "red"}}>{this.state.passwordError}</span>
+                        </div>
+                        </Col>
+                    </Row>
+                    <div className="UserInfo">
+                        <UCPButton className="smallbutton" buttonText="Submit"
+                            type="submit"
+                            onClick={this.handleSubmit}
+                        />
+                    </div>
+                </form>
+            </Container>
+            );
+        }
+    }
+
+   
