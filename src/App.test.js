5b632a3cb297779992de31b76bc503f70687ac77
Niki Heaney <32551966+NickSx8@users.noreply.github.com>
2020-10-28 11:16:50 +0000

    fixed failing tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/PRIVACY STATEMENT/i);
+  const linkElement = getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 
 });
