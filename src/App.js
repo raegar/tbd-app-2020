4373740be03d336912e72653c04238ca6a024ca4
Elliot Reed <elliotsdreed@icloud.com>
2020-11-02 16:18:59 +0000

    fixed tests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Headers from './components/Header/Headers';
-import Calendar from './components/Calendar/Calendar';
-import Disclaimer from './components/Disclaimer/Disclaimer';
+import Login from './components/Login/Login';
+
 
 function App() {
   return (
     <div className="App">
       <Headers/>
-      <Calendar/>
+      <Login/>
       
     </div>
   );
