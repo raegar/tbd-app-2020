02705fa6bdd8d814bdc66a5a5ddda764f70d85a7
Danielle Skutela <Skutela97@gmail.com>
2020-11-05 14:44:54 +0000

    Added Phone no, DOB, UCAS id, and radiobuttons

--- src/components/EssentialApplicantInfo/EssentialApplicantInfo.js
+++ src/components/EssentialApplicantInfo/EssentialApplicantInfo.js
@@ -5,6 +5,13 @@ import UCPButton from '../Buttons/UCPButton'
 import RadioButton from '../RadioButton/RadioButton'
 
 const EssentialApplicantInfo = () => {
+
+    var options = [
+        {label: "UK National", id: "UK", value: "UK"},
+        {label: "EU National", id: "EU", value: "EU"},
+        {label: "Other", id: "Other", value: "Other"}
+    ];
+
     return (
         <div className="form-background">
             <h3>CLEARING APPLICATION</h3>
@@ -26,6 +33,31 @@ const EssentialApplicantInfo = () => {
                 id="emailinput" 
                 type="email" 
             />
+
+            <TextBox 
+                title="Phone Number" 
+                placeholder="Enter phone number" 
+                minLength="5" 
+                maxLength="12" 
+                id="telinput" 
+                type="tel" 
+            />
+
+            <TextBox 
+                title="Date of Birth" 
+                placeholder="Enter date of birth"
+                id="dobinput" 
+                type="date" 
+            />
+
+            <TextBox 
+                title="UCAS ID" 
+                placeholder="Enter UCAS ID" 
+                id="ucasinput" 
+                type="text" 
+            />
+
+            <RadioButton heading="Nationality" options={options} name="test"/>
         </div>
     );
 }
