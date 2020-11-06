c86ee0a4052034da6e8d72fac4c2df57058c1ed2
Abigail Hicks <abigail.hicks@hotmail.co.uk>
2020-11-06 15:20:08 +0000

    Started Creating Request Callback Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import Headers from './components/Header/Headers';
-
-import NewApplicationPage from './NewApplicationPage';
+import RequestCallBackPage from '/Users/abigailhicks/tbd-app-2020-1/src/stories/RequestCallBackPage.js';
 
 function App() {
   return (
     <div className="App">
       <Headers/>
-      <NewApplicationPage/>
+      <RequestCallBackPage/>
       
     </div>
   );
---
+++ src/stories/RequestCallBackPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+import TextBox from '../components/TextBox/TextBox';
+
+
+function RequestCallBackPage () {
+    return (
+   <Container >
+        <TextBox className="TextBox"/>
+      
+      
+      
+      </Container>
+    );
+  }
+  export default RequestCallBackPage;
