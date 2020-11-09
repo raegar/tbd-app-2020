966b244761a631bd51d3a76386bf310027060fe2
Abigail Hicks <abigail.hicks@hotmail.co.uk>
2020-11-09 12:05:36 +0000

    Set Up Page for requesting call back

--- src/stories/RequestCallBackPage.js
+++ src/stories/RequestCallBackPage.js
@@ -6,7 +6,7 @@ import TextBox from '../components/TextBox/TextBox';
 function RequestCallBackPage () {
     return (
    <Container >
-        <TextBox className="TextBox"/>
+        
       
       
       
