91ab848617d20affe406951d0ab5a65a7396b6a3
unknown <usmaan123mahmood@hotmail.com>
2020-11-10 12:05:20 +0000

    added props in jumbotron component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import Headers from './components/Header/Headers';
 import Login from './components/Login/Login';
-
-
 function App() {
   return (
     <div className="App">
