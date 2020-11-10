7ca29ccd7df9278ebf9ad4d9d39fb5e9b5d2b778
Niki Heaney <32551966+NickSx8@users.noreply.github.com>
2020-11-10 14:53:03 +0000

    fixed import error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Headers from './components/Header/Headers';
-import Login from './components/Login/Login';
 import SelectRole from './components/SelectRole/SelectRole';
 import Login from './components/Login/Login'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
