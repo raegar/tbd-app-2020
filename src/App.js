a7d321a20694d7529433fc1e57caec497c95533f
Daniel Dicker <p110094643@student.peterborough.ac.uk>
2020-11-10 16:58:25 +0000

    Removed essential applicant info as it was unused

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Headers from './components/Header/Headers';
 import SelectRole from './components/SelectRole/SelectRole';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import EssentialApplicationInfo from './components/EssentialApplicantInfo/EssentialApplicantInfo'
 import Login from './components/Login/Login'
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 function App() {
