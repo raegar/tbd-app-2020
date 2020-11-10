e8dd13eccf5e72f662ff508fb4ce5eee8b5d9a25
Jamie Hamilton <jamiehamilton81@gmail.com>
2020-11-10 17:41:48 +0000

    Instantiated main form and CSS file

---
+++ src/components/MainForm/MainForm.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import DropdownMenu from '../DropdownMenu/DropdownMenu'
+import TextBox from '../TextBox/TextBox'
+import RadioButton from '../RadioButton/RadioButton'
+import './MainForm.css'
+
+const MainForm = () => {
+
+    return(
+        <div className="form-container">
+        <div className="form-background">
+            <h3 className="form-title">CLEARING FORM</h3>
+
+
+
+        </div>
+        </div>
+    )
+
+}
+
+export default MainForm;
