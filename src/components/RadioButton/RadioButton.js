f02fb8923c1ea0ca46a10ef3f344273d40d966a8
Jamie Hamilton <jamiehamilton81@gmail.com>
2020-11-10 17:11:44 +0000

    Radio button: included subtitles + default props

--- src/components/RadioButton/RadioButton.js
+++ src/components/RadioButton/RadioButton.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import './RadioButton.css'
+import PropTypes from 'prop-types'
 
-const RadioButton = ({name, options, heading, ...props}) => {
+const RadioButton = ({name, options, heading, subtitle, ...props}) => {
+
+    //Iterate through options array and create an input + label for each
 
     var items = options.map((item) =>
         <div className="radio-group">
@@ -10,12 +13,48 @@ const RadioButton = ({name, options, heading, ...props}) => {
         </div>
     );
 
+    //Return div with heading, subtitle and then the items variable which is created in the loop above
+
     return(
         <div className="radio-buttons">
             <h4>{heading}</h4>
+            <p>{subtitle}</p>
             {items}
         </div>
     );
 }
 
+//Prop Types:
+//Note that name and options are required, heading and subtitle are not
+//You must pass an array of JSON objects to instantiate an instance of radio button
+
+RadioButton.propTypes = {
+    name: PropTypes.string.isRequired,
+    options: PropTypes.array.isRequired,
+    heading: PropTypes.string,
+    subtitle: PropTypes.string
+}
+
+//Without providing a heading or subtitle, this values default to null
+
+RadioButton.defaultProps = {
+    heading: null,
+    subtitle: null
+}
+
+//Export
+
 export default RadioButton;
+
+/* 
+
+EXAMPLE USE:
+
+var criminalOptions = [
+        {label: "Yes", id: "crimeYes", value: "yes"},
+        {label: "No", id: "crimeNo", value: "no"}
+    ]
+
+<RadioButton heading="Convictions" options={criminalOptions} name="criminal" subtitle="Do you have any unspent criminal convictions?" />
+
+*/
