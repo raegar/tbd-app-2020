6b3420d6bb18470c59b12f862f9a47c9ded7b9d5
Daniel Dicker <p110094643@student.peterborough.ac.uk>
2020-11-17 12:39:20 +0000

    Amended jumbotron component

--- src/components/ApplicationComplete/ApplicationComplete.js
+++ src/components/ApplicationComplete/ApplicationComplete.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../../stories/Header';
 import {Container} from 'react-bootstrap';
 import JumbotronComponent from '../JumbotronComponent/JumbotronComponent';
-
+import {isMobile} from 'react-device-detect';
 function ApplicationComplete(){
     return(
         <Container>
--- src/components/Jumbotron Component/JumbotronComponent.js
+++ src/components/Jumbotron Component/JumbotronComponent.js
@@ -1,14 +1,22 @@
 import React from 'react';
-export const JumbotronComponent = ({ className, ...props }) => {
+export const JumbotronComponent = ({ className, jumbotronText, ...props }) => {
     return (
         <div className="jumbotron">
             <br></br>
             <div className="jumbotron" data-testid="jumbotroncomponent">
-                <p><b>PRIVACY STATEMENT:</b><br></br> We will only use your personal information to administer your application and / or enrolment and to provide the products and services you have requested from us.<br></br> Further information on our privacy policy can be found at https://www.ucp.ac.uk/privacy-and-cookies/ or by emailing <a href="mailto:hello@ucp.ac.uk" className="text">hello@ucp.ac.uk</a></p>
+                <p>{jumbotronText}</p>
             </div>
         </div>
     )
 }
+JumbotronComponent.propTypes = {
+    className:PropTypes.string,
+    jumbotronText:PropTypes.string
+}
+
+JumbotronComponent.defaultProps = {
+
+  };
 
 export default JumbotronComponent;
 
