c5221564f4bebb090aa3b56c5f4ee7ede2932d2d
Elliot Reed <elliotsdreed@icloud.com>
2020-11-18 14:39:59 +0000

    Header resized and borders created

--- src/components/SelectRole/SelectRole.js
+++ src/components/SelectRole/SelectRole.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container, Row , Col} from 'react-bootstrap';
 import UCPButton from '../Buttons/UCPButton';
-import './SelectRole.css'
+import './SelectRole.css';
+import {isMobile} from 'react-device-detect';
 
 //Must Set HREF attribute to use
 
-const SelectRole = () => {
+const SelectRole = () => {const className = isMobile ? "MobileSelectRole" : "SelectRole";
     return(
         <Container className="mt-5 px-3 py-3 select-role-container">
             <Row className="text-center">
