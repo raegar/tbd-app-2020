3be761bcfb9ab8558a642bbedb7d85e355bd9979
Elliot Reed <elliotsdreed@icloud.com>
2020-11-24 16:40:05 +0000

    Page 14 Created

--- src/components/ClearingOfferMade/ClearingOfferMadeSecondPage copy.js
+++ src/components/ClearingOfferMade/ClearingOfferMadeSecondPage copy.js
@@ -1,4 +1,5 @@
 import React from "react";
+import RadioButton from "../RadioButton/RadioButton";
 
 
 import "./ClearingOfferMade.css";
@@ -8,10 +9,7 @@ import {isMobile} from 'react-device-detect';
 
 const ClearingOfferMadePageThree = (props) => {
 <Container>
-    <p>Offers can only be made if applicant has acheived their qualifications and not if they are waiting for their grade to be published.They need to ring back once their grade has been confirmed </p>
-    <p>If applicant has declared a criminal conviction or applied with prior credit, an offer cannot be made. Inform applicant we will arrange a telephone interview.</p>
-    <p>ALL verbal offers must be prefaced with the words "On the basis of the information you have provided, I am pleased to offer you a place on ..."</p>
-• The applicant must then add UCP as their Clearing Choice via UCAS Track (ideally within 24 hours). If the applicant does not have a UCAS application and they are only wishing to apply to UCP, an RPA form can be completed in place of UCAS - speak to Zoe Rumball </p>
+ 
  </Container>
 
 }
--- src/components/ClearingOfferMade/ClearingOfferMadeThirdPage.js
+++ src/components/ClearingOfferMade/ClearingOfferMadeThirdPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 import "./ClearingOfferMade.css";
 import { Container } from "react-bootstrap";
 import UCPButton from "../Buttons/UCPButton";
@@ -8,6 +7,11 @@ import {isMobile} from 'react-device-detect';
 
 const ClearingOfferMadePageThirdPage = (props) => {
 <Container>
+    <p>Required</p>
+    <h1>Not Offered Details</h1>
+    <h3>38. If NO, please state reasons:</h3>
+    <TextBox/>
+    
  </Container>
 
 }
