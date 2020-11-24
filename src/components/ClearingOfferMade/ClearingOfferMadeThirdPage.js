794cd0decf774fb3ef6b0eb95e46350ccc1fcfaa
Elliot Reed <elliotsdreed@icloud.com>
2020-11-24 16:36:11 +0000

    Clearing Offer First Page Created

---
+++ src/components/ClearingOfferMade/ClearingOfferMadeThirdPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+import "./ClearingOfferMade.css";
+import { Container } from "react-bootstrap";
+import UCPButton from "../Buttons/UCPButton";
+import {isMobile} from 'react-device-detect';
+
+const ClearingOfferMadePageThirdPage = (props) => {
+<Container>
+ </Container>
+
+}
+export default ClearingOfferMadePageThirdPage;
