6499fd5b4e9209dbd8f28d89f14d70dbd9f5ed60
Joshua Morgan <joshuatmorgan95@gmail.com>
2020-11-24 15:26:14 +0000

    added a form title

--- src/components/Degree Qualifications/DegreeQualifications.js
+++ src/components/Degree Qualifications/DegreeQualifications.js
@@ -6,11 +6,27 @@ import UCPButton from "../Buttons/UCPButton";
 import AlerDismissible from '../AlertDismissible/AlertDismissible';
 import {isMobile} from 'react-device-detect';
 
+
 const DegreeQualifications = () => {
     
+    const className = isMobile ? "MobileTextArea" : "DesktopTextArea";  
+    const className1 = isMobile ? "mobile-form-right" : "form-right";
+    const className2 = isMobile ? "mobile-form-container" : "form-container";
+    
+
+    var priorLevelFour = [
+        {label: 'Yes', id: 'prior-yes', value: 'yes'},
+        {label: 'No', id: 'prior-no', value: 'no'}
+    ]
 
     return (
-        
+        <div className={className2}>
+            <div className="form-background">
+                <h3 className="form-title">Level 4 or Above Experience</h3>
+                <AlerDismissible/>
+                <div className={className1}></div>
+            </div>
+        </div>
     );
     
 }
