8bfa2bde2c685cd5bfa623f35da4d4d3a4339ff4
Joshua Morgan <joshuatmorgan95@gmail.com>
2020-11-24 15:13:02 +0000

    Jamie added const Degree Quals

---
+++ src/components/Degree Qualifications/DegreeQualifications.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import DropdownMenu from "../DropdownMenu/DropdownMenu";
+import "./DegreeQualifications.css"; 
+import {Row, Col} from "react-bootstrap";
+import UCPButton from "../Buttons/UCPButton";
+import AlerDismissible from '../AlertDismissible/AlertDismissible';
+import {isMobile} from 'react-device-detect';
+
+const DegreeQualifications = () => {
+    
+
+    return (
+        
+    );
+    
+}
+
+export default DegreeQualifications;
