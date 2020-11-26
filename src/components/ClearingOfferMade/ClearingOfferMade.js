e2d57ce1b01d046ce39bb06738b79e1dc9d90e04
Elliot Reed <elliotsdreed@icloud.com>
2020-11-26 14:44:15 +0000

    Test Error Fixed

--- src/components/ClearingOfferMade/ClearingOfferMade.js
+++ src/components/ClearingOfferMade/ClearingOfferMade.js
@@ -4,7 +4,7 @@ import React from "react";
 import "./ClearingOfferMade.css";
 import { Container, Col } from "react-bootstrap";
 import UCPButton from "../Buttons/UCPButton";
-import {isMobile} from 'react-device-detect';
+
 
 const ClearingOfferMade = (props) => {
   return(
