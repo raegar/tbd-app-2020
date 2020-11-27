1ac2fb687933d367510400f4b932dcff726ce3e4
enasjobs <ernasjobs@gmail.com>
2020-11-27 18:49:47 +0000

    add L3Qualificaions to react router

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './components/Login/Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NewApplicationPage from './NewApplicationPage';
 import LevelTwoQualifications from './components/LevelTwoQualifications/LevelTwoQualifications';
+import LevelThreeQualifications from './components/LevelThreeQualifications/LevelThreeQualifications';
 import ClearingOfferMade from './components/ClearingOfferMade/ClearingOfferMade';
 import DegreeQualifications from './components/Degree Qualifications/DegreeQualifications';
 import ClearingOfferMadeSecondPage from './components/ClearingOfferMade/ClearingOfferMadeSecondPage';
@@ -26,13 +27,12 @@ function App() {
           <Route path='/login' component={Login}/>
           <Route path='/NewApplicationPage' component={NewApplicationPage}/>
           <Route path='/LevelTwoQualifications' component={LevelTwoQualifications}/>
-
+          <Route path='/LevelThreeQualifications' component={LevelThreeQualifications}/>
           <Route path='/ClearingOfferMade' component={ClearingOfferMade}/>
           <Route path='/ClearingOfferSecondPage' component={ClearingOfferMadeSecondPage}/>
           <Route path='/ClearingOfferMadeThirdPage' component={ClearingOfferMadeThirdPage}/>
           <Route path='/ClearingOfferMadeFourthPage' component={ClearingOfferMadeFourthPage}/>
           <Route path='/ClearingOfferMadeFifthPage' component={ClearingOfferMadeFifthPage}/>
-
           <Route path='/DegreeQualifications' component={DegreeQualifications} />
 
         </Switch>
