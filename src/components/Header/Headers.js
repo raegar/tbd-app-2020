5aaf7bb758808be9b6373e05a68fd78a4ce2ebc0
Jamie Hamilton <jamiehamilton81@gmail.com>
2020-12-01 14:45:55 +0000

    Changed to functional component

--- src/components/Header/Headers.js
+++ src/components/Header/Headers.js
@@ -2,16 +2,19 @@ import React from 'react';
 import UCPLogo  from '../../images/UCPLogo.png';
 import './Headers.css';
 import {isMobile} from 'react-device-detect';
-export default class Headers extends React.Component{
-    render(){
-        const className = isMobile ? "MobileUCPLogoBlue" : "UCPLogoBlue";
-        return (
-            <header>   
-                    <a href="/"><img data-testid="ucpLogoHeader" src={UCPLogo} className={className} alt="UCP official logo" /></a>  
-            </header>
-        )
-    }
-};
+
+const Headers = () => {
+
+    const className = isMobile ? "MobileUCPLogoBlue" : "UCPLogoBlue";
+
+    return (
+        <header>   
+                <a href="/"><img data-testid="ucpLogoHeader" src={UCPLogo} className={className} alt="UCP official logo" /></a>  
+        </header>
+    )
+}
+
+export default Headers
 
 
 
