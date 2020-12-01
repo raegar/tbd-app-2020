d3d52359180b45bb75b1c53d6f4c8b52c4b105af
Daniel Dicker <p110094643@student.peterborough.ac.uk>
2020-12-01 14:57:26 +0000

    Typo fixed for the RPA page (required)

--- src/components/ClearingOfferMade/ClearingOfferMadeFourthPage.js
+++ src/components/ClearingOfferMade/ClearingOfferMadeFourthPage.js
@@ -8,7 +8,7 @@ import UCPButton from "../Buttons/UCPButton";
 const ClearingOfferMadeFourthPage = (props) => {
 return (<Container>
 <p>Required</p>
-    <h1>Interview requeired of RPA</h1>
+    <h1>Interview required of RPA</h1>
     <p>Enter any details for decision made</p>
     <h3>39. Please state reasons:</h3>
 
