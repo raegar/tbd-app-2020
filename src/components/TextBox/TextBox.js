883fc2a65963cf6309bfbfc4e9c5e61bb29de256
enasjobs <ernasjobs@gmail.com>
2020-12-06 11:02:33 +0000

    Amended L2 and L3 Qualifications component

--- src/components/TextBox/TextBox.js
+++ src/components/TextBox/TextBox.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './TextBox.css'
 import PropTypes from 'prop-types';
 import {isMobile} from 'react-device-detect';
 
-const TextBox = ({backgroundColor, name, title, size, id, maxLength, minLength, placeholder, type, onChange, min, max, wrapID, attribute,  ...props}) => {
+const TextBox = ({backgroundColor,value, name,key,clearInput, title, size, id, maxLength, minLength, placeholder, type, onChange, min, max, wrapID, attribute,  ...props}) => {
     const className = isMobile ? "mobiletextbox" : "textbox";
- 
+    const[emptyInput, setEmptyInput] = useState(clearInput)
+    const [input, setInput] = useState('');
+    const handleChange = (e) => {
+        if (emptyInput) {setEmptyInput(false)}
+        setInput(e.currentTarget.value);
+        console.log(input);  
+    };
+    const clearInputField = (e) => {
+        setEmptyInput(!clearInput)
+    };
 return (
     <div className={`${className ? "mobiletextbox" : "textbox"}`} id={wrapID}>
         <label htmlFor={id}>{title}</label>
         <input
         name={name}
-        onChange={onChange}
         placeholder={placeholder}
         id= {id}
+        key = {key}
         maxLength = {maxLength}
         minLength = {minLength}
         size={size}
         type={type}
         min={min}
         max={max}
+        value ={emptyInput ? '': input}
         disabled={attribute}
+        onChange = {handleChange}
+        clearInput = {clearInputField}
         />
     </div>
 );
