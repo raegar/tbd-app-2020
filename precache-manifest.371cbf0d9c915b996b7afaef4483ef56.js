297f4cec05d95845c54b68fee0efa26acb9f2639
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-10 18:50:19 +0000

    Deploy raegar/tbd-app-2020 to github.com/raegar/tbd-app-2020.git:gh-pages

---
+++ precache-manifest.371cbf0d9c915b996b7afaef4483ef56.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6addcef3c4e4938b082e5d756e5ee9f",
+    "url": "/tbd-app-2020/index.html"
+  },
+  {
+    "revision": "9a50db16ef76f40e3b8b",
+    "url": "/tbd-app-2020/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f988171aa851a8157799",
+    "url": "/tbd-app-2020/static/css/main.0bca1f66.chunk.css"
+  },
+  {
+    "revision": "9a50db16ef76f40e3b8b",
+    "url": "/tbd-app-2020/static/js/2.0b527636.chunk.js"
+  },
+  {
+    "revision": "8c148efb6436185179e1b7ec9806bfdd",
+    "url": "/tbd-app-2020/static/js/2.0b527636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f988171aa851a8157799",
+    "url": "/tbd-app-2020/static/js/main.d39795bf.chunk.js"
+  },
+  {
+    "revision": "3e2c9d8f5b64489a328b",
+    "url": "/tbd-app-2020/static/js/runtime-main.9c11f291.js"
+  },
+  {
+    "revision": "ab926c9f121ffd1bca17868172e97b01",
+    "url": "/tbd-app-2020/static/media/Footer.ab926c9f.png"
+  },
+  {
+    "revision": "0c4305788396b0587411a6ab02863c96",
+    "url": "/tbd-app-2020/static/media/UCPLogo.0c430578.png"
+  }
+]);
