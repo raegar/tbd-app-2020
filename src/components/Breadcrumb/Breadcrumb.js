e854a1aa9eca35958d1c0ce76873e85eb0191939
Coldani3 <p110117069@gmail.com>
2021-10-13 19:28:18 +0100

    Convert and fix DegreeQualification, Checkbox, Calendar and Breadcrumb.

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import { Container} from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import './Breadcrumb.css';
 //import { Breadcrumb } from 'react-bootstrap';
 
-
-export default class BreadcrumbComp extends React.Component{
-    render(){
-        return (        
-
-
-
-            
-            
-            <Container>
-<ul class="breadcrumb">
-  <li><a href="www.google.co.uk">Personal Information</a></li>
-  <li><a href="www.google.co.uk">Qualifications</a></li>
-  <li><a href="www.google.co.uk">About you</a></li>
-</ul>
-
-
-
-            </Container>
-        )
-    }
+class BreadcrumbComp extends React.Component{
+	render = () => {
+		return (
+			<Container>
+				<ul class="breadcrumb">
+					<li><a href="www.google.co.uk">Personal Information</a></li>
+					<li><a href="www.google.co.uk">Qualifications</a></li>
+					<li><a href="www.google.co.uk">About you</a></li>
+				</ul>
+			</Container>
+		);
+	}
 };
+
+export default BreadcrumbComp;
--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -3,26 +3,33 @@ import PropTypes from 'prop-types';
 import './Checkbox.css';
 
 
-export const Checkbox = ({className, checkboxText, ...props}) => {    
-    return(
-        <div className="labelposition">
-            <input className={className} type="checkbox" />
-            <div className="checkboxposition">
-                <label className="container">{checkboxText}</label>
-            </div>           
-        </div>
-    )
+class Checkbox extends React.Component {
+	constructor(props) {
+		super(props);
+		this.props = props;
+	}
+
+    render = () => {
+    	return(
+    		<div className="labelposition">
+    			<input className={this.props.className} type="checkbox" />
+    			<div className="checkboxposition">
+    				<label className="container">{this.props.checkboxText}</label>
+    			</div>           
+    		</div>
+    	);
+    }
 }
   
   
 Checkbox.propTypes = {
-    className:PropTypes.string,
-    checkboxText:PropTypes.string
-}
+	className:PropTypes.string,
+	checkboxText:PropTypes.string
+};
 
-  Checkbox.defaultProps = {
+Checkbox.defaultProps = {
 
-  };
+};
 
 
-  export default Checkbox
+export default Checkbox;
