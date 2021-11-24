ec01057cb26cfcd0e7d318d1f7001c1834c308ac
Coldani3 <p110117069@gmail.com>
2021-11-24 10:15:58 +0000

    Move various consts to the Constants file.

--- src/global/Constants.js
+++ src/global/Constants.js
@@ -1,4 +1,54 @@
-export const Subjects = [{ value: 'computing', label: 'BSc (Hons) Computing and Information Systems' },
-	{ value: 'criminoloy', label: 'BA (Hons) Crimonology'},
-	{ value: 'digitalArts', label: 'FDA Digital Arts'}
+export const Time = [
+	{value: 'morning', label: '09:00am - 12:00pm'  },
+	{value: 'afternoon', label: '12:00pm - 15:00pm'},
+	{value: 'evening', label: '15:00pm - 17:00pm'  },
 ];
+
+export const TimeOptions = [
+	{ value: '0', label: '9:00' },
+	{ value: '1', label: '10:00'},
+	{ value: '2', label: '11:00'},
+	{ value: '3', label: '12:00'},
+	{ value: '4', label: '13:00' },
+	{ value: '5', label: '14:00'},
+	{ value: '6', label: '15:00'},
+	{ value: '7', label: '16:00'},
+];
+
+export const Days = [
+	{value: 'monday', label: 'Monday'      },
+	{value: 'tuesday', label: 'Tuesday'    },
+	{value: 'wednesday', label: 'Wednesday'},
+	{value: 'thursday', label: 'Thursday'  },
+	{value: 'friday', label: 'Friday'      },
+];
+
+export const L3QualificationsOptions = [
+	{ label: "Yes", id: "qualificationYes", value: "yes" },
+];
+
+export const Subjects = [{ value: 'computing', label: 'BSc (Hons) Computing and Information Systems' },
+	{ value: 'criminoloy', label: 'BA (Hons) Crimonology'                                            },
+	{ value: 'digitalArts', label: 'FDA Digital Arts'                                                },
+];
+
+export const GCSEGrades = [
+	{ value: "9", label: "9 - A*" },
+	{ value: "8", label: "8 - A*" },
+	{ value: "7", label: "7 - A"  },
+	{ value: "6", label: "6 - B"  },
+	{ value: "5", label: "5 - B"  },
+	{ value: "4", label: "4 - C"  },
+	{ value: "3", label: "3 - D"  },
+	{ value: "2", label: "2 - E"  },
+	{ value: "1", label: "2 - F"  },
+	{ value: "1", label: "1 - G"  },
+	{ value: "u", label: "U"      },
+	{ value: "na", label: "n/a"   },
+];
+
+export const QualificationKey = "qualification";
+export const SubjectKey = "subject";
+export const GradeKey = "grade";
+export const YearKey = "year";
+
