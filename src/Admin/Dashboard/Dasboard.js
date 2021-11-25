12e8312d411f88a89fed26cd27f70543ffa61c8a
Jessica <jessiemarydow@hotmail.co.uk>
2021-11-25 11:17:48 +0000

    Updated Tab component and moved files

---
+++ src/Admin/Dashboard/Dasboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Renderer from '../TabbedNavbar/Renderer';
+
+const ClearingOfferMade = (props) =>{
+    return(
+        <Container>
+            <Renderer/>
+         </Container>
+    )}
+
+export default Dashboard;
