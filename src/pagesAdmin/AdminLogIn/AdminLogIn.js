da983d7662aeacec330239c94103bf2996d9b43a
Jamie Myland <jamie.myland@peterborough.ac.uk>
2021-11-30 13:30:05 +0000

    Removed reference to useState as it is now unused

--- src/pagesAdmin/AdminLogIn/AdminLogIn.js
+++ src/pagesAdmin/AdminLogIn/AdminLogIn.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Row, Col} from 'react-bootstrap';
 import './AdminLogIn.css';
 import TextBox from '../../components/TextBox/TextBox';
