014d7664cc541750af17f0e1288e92cc249b7d33
Coldani3 <p110117069@gmail.com>
2021-12-01 13:41:50 +0000

    The functionality all seems to be in place and working. It will break the instant it is shown off but that is to be expected.

--- src/components/UcasCalculator/UcasCalculator.js
+++ src/components/UcasCalculator/UcasCalculator.js
@@ -4,10 +4,12 @@ import {UCASGrades} from '../../global/Constants';
 import TextBox from '../TextBox/TextBox';
 import DropdownMenu from '../DropdownMenu/DropdownMenu';
 import {Container, Row, Col} from "react-bootstrap";
+import UCPButton from '../Buttons/UCPButton';
 
 function UcasCalculator() {
 	const [selectedGrades, setSelectedGrades] = useState([{name: "", points: 0}]);
 	const [gradeOptions, setGradeOptions] = useState([]);
+	const [gradesCount, setGradesCount] = useState(1);
 
 	function gradesOptions() {
 		const options = [];
@@ -25,13 +27,16 @@ function UcasCalculator() {
 		return options;
 	}
 
-	function selectGradeType(e) {
+	function selectGradeType(e, index) {
 		const split = e.target.value.split(";");
 		const name = split[0];
 		const subgradeName = split[1];
 		const data = UCASGrades[name][subgradeName];
 
-		setGradeOptions(data);
+		const gradeOptionsCopy = gradeOptions;
+		gradeOptionsCopy[index] = data;
+
+		setGradeOptions([...gradeOptionsCopy]);
 	}
 
 	function changeSubjectName(e, index) {
@@ -62,25 +67,29 @@ function UcasCalculator() {
 		return sum;
 	}
 
-	return (
-		<div>
-			<Container>
+	function generateUcasGrades() {
+		let grades = [];
+
+		for (let i = 0; i < gradesCount; i++) {
+			grades.push(
 				<Row>
 					<Col>
-						<select id={"qualifications"} onChange={selectGradeType}>
+						<select id={"qualifications"} onChange={(e) => selectGradeType(e, i)}>
 							<option disabled selected="selected">Please select a qualification...</option>
 							{gradesOptions()}
 						</select>
 					</Col>
 				</Row>
+			);
+			grades.push(
 				<Row>
 					<Col>
-						<TextBox placeholder="Subject (optional)" onChange={(e) => changeSubjectName(e, 0)}/>
+						<TextBox placeholder="Subject (optional)" onChange={(e) => changeSubjectName(e, i)}/>
 					</Col>
 					<Col>
 						<DropdownMenu 
-							options={gradeOptions.length > 0 ?
-								gradeOptions.map((option, i) => {
+							options={gradeOptions.length > 0 && gradeOptions[i] ?
+								gradeOptions[i].map((option, j) => {
 									return {
 										label: option.grade,
 										value: option.points
@@ -89,15 +98,35 @@ function UcasCalculator() {
 								: [{label: "...", value: "none"}]
 							}
 							disabled={gradeOptions.length <= 0}
-							onSelect={(e) => selectGrade(e, 0)}
+							onSelect={(e) => selectGrade(e, i)}
 						/>
 					</Col>
 				</Row>
+			);
+			grades.push(
 				<Row>
 					<Col/>
 					<Col>
-						<span>Points <b>{selectedGrades[0]?.points}</b></span>
+						<span>Points <b>{selectedGrades[i]?.points}</b></span>
 					</Col>
+				</Row>
+			);
+		}
+
+		return grades;
+	}
+
+	return (
+		<div>
+			<Container>
+				{generateUcasGrades()}
+				<Row>
+					<UCPButton 
+						to="none"
+						className="mediumbutton" 
+						onClick={() => setGradesCount(gradesCount + 1)} 
+						buttonText="Add another qualification"
+					/>
 				</Row>
 				<span>Total points <b>{totalPoints()}</b></span>
 			</Container>
