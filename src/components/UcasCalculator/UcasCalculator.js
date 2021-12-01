05eb06304d59ee715fd093f1d7ca99b4d8eb401d
Coldani3 <p110117069@gmail.com>
2021-12-01 10:24:33 +0000

    Add boilerplate stuff and sample constants for the UCAS grades, formatted in a similar way to the UCAS calculator site.

---
+++ src/components/UcasCalculator/UcasCalculator.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import './UcasCalculator.css'
+import { UCASGrades } from '../../global/Constants';
+import {Container} from 'react-bootstrap';
+
+function UcasCalculator({...props}) {
+    const [selectedGrades, setSelectedGrades] = useState([{}]);
+    return (<Container>
+
+    </Container>);
+}
+
+export default UcasCalculator;
--- src/global/Constants.js
+++ src/global/Constants.js
@@ -62,6 +62,28 @@ export const GCSEGrades = [
 	{ value: "na", label: "n/a"   },
 ];
 
+export const UCASGrades = {
+	"A Level and AS": {
+		"A Levels": {
+			"A*": 56,
+			"A": 48,
+			"B": 40,
+			"C": 32,
+			"D": 24,
+			"E": 16
+		},
+		"AS Levels": {}
+	},
+	"BTEC National (RQF) (From 2016)": {
+		"Pearson BTEC Level 3 National Extended Certificate (first teaching from September 2016)": {
+			"D*": 56,
+			"D": 48,
+			"M": 32,
+			"P": 24
+		}
+	}
+}
+
 export const QualificationKey = "qualification";
 export const SubjectKey = "subject";
 export const GradeKey = "grade";
