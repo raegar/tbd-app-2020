736325138d5221a8e19aac5d5d278e9d4de616c0
Coldani3 <p110117069@gmail.com>
2021-12-01 13:12:15 +0000

    Calculator is kind of working but not really.

--- src/components/UcasCalculator/UcasCalculator.js
+++ src/components/UcasCalculator/UcasCalculator.js
@@ -6,7 +6,7 @@ import DropdownMenu from '../DropdownMenu/DropdownMenu';
 import {Container, Row, Col} from "react-bootstrap";
 
 function UcasCalculator() {
-	//const [selectedGrades, setSelectedGrades] = useState([{}]);
+	const [selectedGrades, setSelectedGrades] = useState([{name: "", points: 0}]);
 	const [gradeOptions, setGradeOptions] = useState([]);
 
 	function gradesOptions() {
@@ -18,7 +18,7 @@ function UcasCalculator() {
 
 			//now add each subgrade type
 			for (const subgradeName in UCASGrades[name]) {
-				options.push(<option value={UCASGrades[name][subgradeName]}>{"  " + subgradeName}</option>);
+				options.push(<option value={name + ";" + subgradeName}>{"  " + subgradeName}</option>);
 			}
 		}
 
@@ -26,8 +26,46 @@ function UcasCalculator() {
 	}
 
 	function selectGradeType(e) {
-		console.log(JSON.stringify(e.target.value));
-		setGradeOptions(e.target.value);
+		const split = e.target.value.split(";");
+		const name = split[0];
+		const subgradeName = split[1];
+		const data = UCASGrades[name][subgradeName];
+
+		setGradeOptions(data);
+	}
+
+	function changeSubjectName(e, index) {
+		let gradesCopy = selectedGrades;
+		gradesCopy[index].name = e.target.value;
+		console.log("subject change");
+		setSelectedGrades(gradesCopy);
+	}
+
+	function selectGrade(e, index) {
+		let gradesCopy = selectedGrades;
+		console.log(e.target.value);
+
+		if (!gradesCopy[index]) {
+			gradesCopy[index] = {name: "", points: parseInt(e.target.value)};
+		} else {
+			gradesCopy[index].points = parseInt(e.target.value);
+		}
+
+		setSelectedGrades(gradesCopy);
+		console.log(JSON.stringify(selectedGrades));
+		console.log(selectedGrades[index].points);
+	}
+
+	function totalPoints() {
+		let sum = 0;
+
+		for (let i = 0; i < selectedGrades.length; i++) {
+			sum += selectedGrades[i].points;
+		}
+
+		console.log("sum");
+
+		return sum;
 	}
 
 	return (
@@ -35,7 +73,7 @@ function UcasCalculator() {
 			<Container>
 				<Row>
 					<Col>
-						<select id={"grades"} onChange={selectGradeType}>
+						<select id={"qualifications"} onChange={selectGradeType}>
 							<option disabled selected="selected">Please select a qualification...</option>
 							{gradesOptions()}
 						</select>
@@ -43,19 +81,34 @@ function UcasCalculator() {
 				</Row>
 				<Row>
 					<Col>
-						<TextBox placeholder="Subject (optional)"/>
+						<TextBox placeholder="Subject (optional)" onChange={(e) => changeSubjectName(e, 0)}/>
 					</Col>
 					<Col>
-						<DropdownMenu options={
-							gradeOptions.map((option, i) => {
-								return {
-									label: option.grade,
-									value: option.points
-								};
-							})
-						}/>
+						<DropdownMenu 
+							options={gradeOptions.length > 0 ?
+								gradeOptions.map((option, i) => {
+									return {
+										label: option.grade,
+										value: option.points
+									};
+								}) 
+								: [{label: "...", value: "none"}]
+							}
+							disabled={gradeOptions.length <= 0}
+							onSelect={(e) => {
+								console.log("test"); 
+								selectGrade(e, 0);
+							}}
+						/>
 					</Col>
 				</Row>
+				<Row>
+					<Col/>
+					<Col>
+						<span>Points <b>{selectedGrades[0]?.points}</b></span>
+					</Col>
+				</Row>
+				<span>Total points <b>{totalPoints()}</b></span>
 			</Container>
 		</div>
 	);
