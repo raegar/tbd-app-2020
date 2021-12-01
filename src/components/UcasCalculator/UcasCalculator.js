690a1449d5c20569620e5defd8a8244e69a760ea
Coldani3 <p110117069@gmail.com>
2021-12-01 10:45:40 +0000

    Small stuff, adding Storybook file.

--- src/components/UcasCalculator/UcasCalculator.js
+++ src/components/UcasCalculator/UcasCalculator.js
@@ -2,12 +2,20 @@ import React, {useState} from 'react';
 import './UcasCalculator.css'
 import { UCASGrades } from '../../global/Constants';
 import {Container} from 'react-bootstrap';
+import {TextBox} from '../TextBox/TextBox';
+import {DropdownMenu} from '../DropdownMenu/DropdownMenu';
 
-function UcasCalculator({...props}) {
-    const [selectedGrades, setSelectedGrades] = useState([{}]);
-    return (<Container>
-
-    </Container>);
+function UcasCalculator() {
+    //const [selectedGrades, setSelectedGrades] = useState([{}]);
+    return (
+        <Container>
+            <select>
+                <option name="Test" value="Test"/>
+            </select>
+            <TextBox placeholder="Subject (optional)"/>
+            <DropdownMenu/>
+        </Container>
+    );
 }
 
 export default UcasCalculator;
