9cc4b2b82d02066d642a1c7b4c8d89b4d9b26dcd
Krystian P110112028 <P110112028@student.peterborough.ac.uk>
2021-12-02 11:22:05 +0000

    Added a button to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import './Footer.css';
 import {isMobile} from 'react-device-detect';
+import { Container, Row, Col } from "react-bootstrap";
+import UCPButton from "../Buttons/UCPButton";
+import Modal from "../Modal/Modal";
+import DropdownMenu from "../DropdownMenu/DropdownMenu";
+import {Time, Days} from '../../global/Constants';
 
 function Footer() {
 	const className = isMobile ? "MobileFooter" : "ucpFooter";
 
-	return (
-		<footer className={className}/>   
+					
+	return (	
+		<div><footer className={className}/>
+			
+						<div id="confirmButton1">
+							<Modal id="hello" btnText="Request a Call Back" className = "CallbackFooter">
+								<div>
+									<h2>Callback Request</h2>
+									<p>Please select a day and time you would be available.</p>
+									<Row id="dropdownRow">
+										<Col className="dropdownOptions1">
+											<DropdownMenu placeholder="Please select a day..." options={Days}/>
+										</Col>
+										<Col className="dropdownOptions2">
+											<DropdownMenu placeholder="Please select a time..." options={Time}/>
+										</Col>
+									</Row>
+									<br/>
+									<UCPButton 
+										to="none"
+										primary="True"
+										className="smallbutton"
+										buttonText="Submit" 
+									/>
+								</div>
+							</Modal>
+						</div>
+
+		</div>
+
 	);
+	
 };
 
 export default Footer;
