bb8369477e79c35b52b61370ac1aa2f60f00e077
Coldani3 <Coldani3@users.noreply.github.com>
2021-12-02 11:36:52 +0000

    deploy: f1a93f32afe65c18fb9497cdf988baf380f56bb2

---
+++ precache-manifest.a03a9757ca2ce4973b90052a3045bddc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da612f36ce086fb8a9a81d0c8700d5ef",
+    "url": "/tbd-app-2020/index.html"
+  },
+  {
+    "revision": "0bc98a83d2f36c65b2fb",
+    "url": "/tbd-app-2020/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "ded6e4a5b3780a5134aa",
+    "url": "/tbd-app-2020/static/css/main.e34a2dbc.chunk.css"
+  },
+  {
+    "revision": "0bc98a83d2f36c65b2fb",
+    "url": "/tbd-app-2020/static/js/2.f6e51f92.chunk.js"
+  },
+  {
+    "revision": "3975064d7735ae6d64a08870a919e367",
+    "url": "/tbd-app-2020/static/js/2.f6e51f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded6e4a5b3780a5134aa",
+    "url": "/tbd-app-2020/static/js/main.682fcead.chunk.js"
+  },
+  {
+    "revision": "3e2c9d8f5b64489a328b",
+    "url": "/tbd-app-2020/static/js/runtime-main.9c11f291.js"
+  },
+  {
+    "revision": "ab926c9f121ffd1bca17868172e97b01",
+    "url": "/tbd-app-2020/static/media/Footer.ab926c9f.png"
+  },
+  {
+    "revision": "cc8e2a656a234555ba66e2feda2e7773",
+    "url": "/tbd-app-2020/static/media/Logo_White.cc8e2a65.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tbd-app-2020/precache-manifest.aaccd8e6619fc6286fd6434ea59a5cc9.js"
+  "/tbd-app-2020/precache-manifest.a03a9757ca2ce4973b90052a3045bddc.js"
 );
 
 self.addEventListener('message', (event) => {
