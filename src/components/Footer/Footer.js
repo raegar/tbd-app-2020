b4b5dee162965e5e9195257b685279cee2817f6b
Viktorija Garifova <kairotv21@gmail.com>
2021-12-02 11:27:01 +0000

    fixing error

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,32 +12,30 @@ function Footer() {
 
 					
 	return (	
-		<div><footer className={className}/>
-			
-						<div id="confirmButton1">
-							<Modal id="hello" btnText="Request a Call Back" className = "CallbackFooter">
-								<div>
-									<h2>Callback Request</h2>
-									<p>Please select a day and time you would be available.</p>
-									<Row id="dropdownRow">
-										<Col className="dropdownOptions1">
-											<DropdownMenu placeholder="Please select a day..." options={Days}/>
-										</Col>
-										<Col className="dropdownOptions2">
-											<DropdownMenu placeholder="Please select a time..." options={Time}/>
-										</Col>
-									</Row>
-									<br/>
-									<UCPButton 
-										to="none"
-										primary="True"
-										className="smallbutton"
-										buttonText="Submit" 
-									/>
-								</div>
-							</Modal>
+		<div>
+			<footer className={className}>
+					<Modal id="hello" btnText="Request a Call Back" className = "CallbackFooter">
+						<div>
+							<h2>Callback Request</h2>
+							<p>Please select a day and time you would be available.</p>
+							<Row id="dropdownRow">
+								<Col className="dropdownOptions1">
+									<DropdownMenu placeholder="Please select a day..." options={Days}/>
+								</Col>
+								<Col className="dropdownOptions2">
+									<DropdownMenu placeholder="Please select a time..." options={Time}/>
+								</Col>
+							</Row>
+							<br/>
+							<UCPButton 
+								to="none"
+								primary="True"
+								className="smallbutton"
+								buttonText="Submit" 
+							/>
 						</div>
-
+					</Modal>
+			</footer>
 		</div>
 
 	);
