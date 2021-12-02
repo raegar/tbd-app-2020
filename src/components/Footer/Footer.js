fba06cf4d82ac3a4bc00b96632f9f44e44729a41
Krystian P110112028 <P110112028@student.peterborough.ac.uk>
2021-12-02 11:45:48 +0000

    Removed un-used Imports + indent change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './Footer.css';
 import {isMobile} from 'react-device-detect';
-import { Container, Row, Col } from "react-bootstrap";
-import UCPButton from "../Buttons/UCPButton";
-import Modal from "../Modal/Modal";
-import DropdownMenu from "../DropdownMenu/DropdownMenu";
-import {Time, Days} from '../../global/Constants';
+
 
 function Footer() {
 	const className = isMobile ? "MobileFooter" : "ucpFooter";
 
 					
 	return (	
-			<footer className={className}/>
+		<footer className={className}/>
 	);
 	
 };
