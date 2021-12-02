39f2c69ffc589954bdbb8a5d9dea7ac07f2ee3cb
Coldani3 <p110117069@gmail.com>
2021-12-02 12:02:48 +0000

    Add embed toggle button.

--- src/components/CourseInfoPanel/CourseInfoPanel.js
+++ src/components/CourseInfoPanel/CourseInfoPanel.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./CourseInfoPanel.css";
 import { Row, Container } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCommentDots } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 import { SubjectInfo } from '../../global/Constants';
+import UCPButton from '../Buttons/UCPButton';
 
 
 function CourseInfoPanel({courseSelected, className, ...props}) {
+	const [showEmbed, setShowEmbed] = useState(true);
 
 	return (
 		<Container className={"courseInfoBox " + className}>
@@ -17,12 +19,21 @@ function CourseInfoPanel({courseSelected, className, ...props}) {
 			</Row>
 			<Row className="uniPageLinkRow">
 				{/* Uni page link */}
-				<iframe className="iframeLink" src="https://www.ucp.ac.uk/a-z-courses/" title="Courses at UCP"/>
+				<UCPButton 
+					to="none" 
+					onClick={() => setShowEmbed(!showEmbed)} 
+					buttonText={(showEmbed ? "Close" : "Open") + " Embed"}
+					className="smallbutton"
+				/>
+				{showEmbed ? <div className="iframeContainer">
+					<iframe className="iframeLink" src="https://www.ucp.ac.uk/a-z-courses/" title="Courses at UCP"/>
+				</div> : null}
+			</Row>
+			<Row>
 				<a href="https://www.ucp.ac.uk/a-z-courses/">Link to Course information page on UCP website</a>
 			</Row>
 			{courseSelected ? <Row className="courseInfoRow">
-				<span><b>Course Overview:</b></span>
-				{SubjectInfo[courseSelected]}
+				<span><b>Course Overview: </b>{SubjectInfo[courseSelected]}</span>
 			</Row> : null}
 		</Container>
 	);
