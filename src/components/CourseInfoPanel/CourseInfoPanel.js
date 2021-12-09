213fa8ebff20d816cc3ab860b8a3cf207fa0f558
Coldani3 <p110117069@gmail.com>
2021-12-09 11:21:46 +0000

    Add button to move calculator from bottom to top and back.

--- src/components/CourseInfoPanel/CourseInfoPanel.js
+++ src/components/CourseInfoPanel/CourseInfoPanel.js
@@ -12,12 +12,31 @@ import UcasCalculator from '../UcasCalculator/UcasCalculator';
 
 function CourseInfoPanel({className, ...props}) {
 	const [showEmbed, setShowEmbed] = useState(true);
+	const [ucasCalcAtTop, setUcasCalcPos] = useState(false);
+
+	function genUcasCalculator() {
+		return (<Row className="ucasCalculatorRow">
+			<p><b>UCAS Calculator:</b></p>
+			<div className="ucasCalculator">
+				<UcasCalculator/>
+			</div>
+		</Row>);
+	}
 
 	return (
 		<div className={"courseInfoBox " + className}>
 			<Row className="courseTitleRow">
 				{/* Title */}
 				<h1 className="courseInfoTitle"><FontAwesomeIcon className="icon" icon={faCommentDots}/>  Course Info</h1>
+			</Row>
+			<Row>
+				<UCPButton
+					to="none"
+					onClick={() => setUcasCalcPos(!ucasCalcAtTop)}
+					buttonText={"Move UCAS Calculator to " + (!ucasCalcAtTop ? "Here" : "Bottom")}
+					className="mediumbutton"
+				/>
+				{ucasCalcAtTop ? genUcasCalculator() : null}
 			</Row>
 			<Row className="uniPageLinkRow">
 				{/* Uni page link */}
@@ -37,12 +56,7 @@ function CourseInfoPanel({className, ...props}) {
 			{global.SelectedCourse ? <Row className="courseInfoRow">
 				<span><b>Course Overview: </b>{SubjectInfo[global.SelectedCourse]}</span>
 			</Row> : null}
-			<Row className="ucasCalculatorRow">
-				<p><b>UCAS Calculator:</b></p>
-				<div className="ucasCalculator">
-					<UcasCalculator/>
-				</div>
-			</Row>
+			{ucasCalcAtTop ? null : genUcasCalculator()}
 		</div>
 	);
 }
