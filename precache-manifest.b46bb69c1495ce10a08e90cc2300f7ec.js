2496578937730208b7d962f09f1981704fc9fee8
HachyCode <HachyCode@users.noreply.github.com>
2021-12-15 14:25:31 +0000

    deploy: 7904a462f6a823350af7757a514a38986733bc6f

--- precache-manifest.b46bb69c1495ce10a08e90cc2300f7ec.js
+++ precache-manifest.b46bb69c1495ce10a08e90cc2300f7ec.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4c271423dcb7e57c725273dd927e7238",
+    "revision": "ff0cdc22055fae04a67cf64f4cab54f7",
     "url": "/tbd-app-2020/index.html"
   },
   {
-    "revision": "d7093bf8734c27179563",
+    "revision": "bd5063b1be92aaa7436c",
     "url": "/tbd-app-2020/static/css/2.429e79c4.chunk.css"
   },
   {
-    "revision": "2a4f605d3285746a6c8b",
+    "revision": "b1713643ac26355d2f80",
     "url": "/tbd-app-2020/static/css/main.c58d069c.chunk.css"
   },
   {
-    "revision": "d7093bf8734c27179563",
-    "url": "/tbd-app-2020/static/js/2.ef011e9c.chunk.js"
+    "revision": "bd5063b1be92aaa7436c",
+    "url": "/tbd-app-2020/static/js/2.1edb11b6.chunk.js"
   },
   {
     "revision": "3975064d7735ae6d64a08870a919e367",
-    "url": "/tbd-app-2020/static/js/2.ef011e9c.chunk.js.LICENSE.txt"
+    "url": "/tbd-app-2020/static/js/2.1edb11b6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2a4f605d3285746a6c8b",
-    "url": "/tbd-app-2020/static/js/main.34bd7ddd.chunk.js"
+    "revision": "b1713643ac26355d2f80",
+    "url": "/tbd-app-2020/static/js/main.1b685a01.chunk.js"
   },
   {
     "revision": "3e2c9d8f5b64489a328b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tbd-app-2020/precache-manifest.06b8c32272ca5c3d3b0e81337b122175.js"
+  "/tbd-app-2020/precache-manifest.b46bb69c1495ce10a08e90cc2300f7ec.js"
 );
 
 self.addEventListener('message', (event) => {
